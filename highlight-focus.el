dd272a3b125e38a3a3f8072e038c8e14d222144b
Ram Krishnan <kriyative@gmail.com>
2017-10-25 12:07:42 -0700

    Initial commit

---
+++ highlight-focus.el
@@ -0,0 +1,61 @@
+;;; highlight-focus.el --- highlight the active buffer
+
+;; Author: Amit J Patel <amitp@cs.stanford.edu>
+
+;;; Commentary:
+;; 
+;; I find that I'm not good at tracking when focus changes across
+;; apps, windows, and within a window. As much as possible, I try to
+;; have all my applications somehow draw attention to what has
+;; focus. In X11 I marked the focus in red. In Firefox I marked the
+;; text fields in yellow. This Emacs package highlights the active
+;; buffer. It's inspired by an earlier package I had written for
+;; XEmacs, which changes the window color and modeline color for the
+;; current window.
+;;
+;;; History:
+;;
+;; 2014-05-07: Updated to use the Emacs 24 focus-{in,out}-hook
+;; 2013-05-10: Rewritten to use the Emacs 23 "remap faces" feature.
+;; 2007-04-16: Initial version, temporarily highlighting the active buffer
+
+;; Also see <https://github.com/emacsmirror/auto-dim-other-buffers>
+
+;;; Code:
+
+(require 'face-remap)
+(defvar highlight-focus:last-buffer nil)
+(defvar highlight-focus:cookie nil)
+(defvar highlight-focus:background "white")
+(defvar highlight-focus:app-has-focus t)
+
+(defun highlight-focus:check ()
+  "Check if focus has changed, and if so, update remapping."
+  (let ((current-buffer (and highlight-focus:app-has-focus (current-buffer))))
+    (unless (eq highlight-focus:last-buffer current-buffer)
+      (when (and highlight-focus:last-buffer highlight-focus:cookie)
+        (with-current-buffer highlight-focus:last-buffer
+          (face-remap-remove-relative highlight-focus:cookie)))
+      (setq highlight-focus:last-buffer current-buffer)
+      (when current-buffer
+        (setq highlight-focus:cookie
+              (face-remap-add-relative 'default :background highlight-focus:background))))))
+
+(defun highlight-focus:app-focus (state)
+  (setq highlight-focus:app-has-focus state)
+  (highlight-focus:check))
+
+(defadvice other-window (after highlight-focus activate)
+  (highlight-focus:check))
+(defadvice select-window (after highlight-focus activate)
+  (highlight-focus:check))
+(defadvice select-frame (after highlight-focus activate)
+  (highlight-focus:check))
+(add-hook 'window-configuration-change-hook 'highlight-focus:check)
+
+(add-hook 'focus-in-hook (lambda () (highlight-focus:app-focus t)))
+(add-hook 'focus-out-hook (lambda () (highlight-focus:app-focus nil)))
+
+(provide 'highlight-focus)
+
+;;; highlight-focus.el ends here
